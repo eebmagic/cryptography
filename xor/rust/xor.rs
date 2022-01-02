24424cd187361bd8843c50c8ade1840ba724a085
E Bolton <eebmagic@gmail.com>
2022-01-01 23:03:45 -0600

    read/write file as bytes

--- xor/rust/xor.rs
+++ xor/rust/xor.rs
@@ -1,44 +1,35 @@
-use std::fs;
+use std::io;
+use std::io::Read;
+use std::io::BufReader;
+use std::io::BufWriter;
+use std::io::Write;
+use std::fs::File;
 
-fn print_type_of<T>(_: &T) {
-    println!("{}", std::any::type_name::<T>())
-}
+fn main() -> io::Result<()> {
+    // Set key values
+    let key: u8 = 123;
+    let filename = "source.txt";
 
-fn xor_str(s: &String, key: u8) -> String {
-    let mut result = String::from("");
-    for c in s.chars() {
-        let new = ((c as u8)^key) as char;
-        result.push(new);
-        // println!("{}, {} -> {}", i, c, new);
+    // Setup file readers
+    let f = File::open(filename)?;
+    let mut reader = BufReader::new(f);
+    let mut buffer = Vec::new();
+    
+    // Read file into vector.
+    reader.read_to_end(&mut buffer)?;
+    
+    // Read
+    let mut vec = Vec::new();
+    for value in buffer {
+        let newvalue = value ^ key;
+        println!("BYTE: {}, {} -> {}", value, value as char, newvalue as char);
+        vec.push(newvalue);
     }
 
-    // println!("result: {}", result);
-    return result;
-}
+    let c: &[u8] = &vec;
+    let mut result_buffer = BufWriter::new(File::create("output.txt")?);
+    result_buffer.write(c)?;
+    result_buffer.flush()?;
 
-fn main() {
-    // Set key value
-    let key: u8 = 123;
-
-    // Load file content
-    let filename = "source.txt";
-    println!("filename: {}", filename);
-    let contents = fs::read_to_string(filename)
-        .expect("Something went wrong reading the file");
-
-    println!("Found text:\n{}", contents);
-    print_type_of(&contents);
-
-
-    // Iterate over content
-    let result = xor_str(&contents, key);
-    let decrypt = xor_str(&result, key);
-    println!("\n---------\nENCRYPTING\n---------\n");
-    println!("out of encrypt:\n{}", result);
-
-    println!("\n---------\nDECRYPTING\n---------\n");
-    println!("out of decrypt:\n{}", decrypt);
-
-    // Write to file
-    fs::write("output.txt", result).expect("Unable to write file");
+    Ok(())
 }
